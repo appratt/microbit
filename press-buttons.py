8e6959399dfcc2fb1ecd0f05bfab7f004e1f44f9
Andrew Pratt <appratt@gmail.com>
2018-03-03 20:55:30 -0500

    Update press-buttons.py

--- press-buttons.py
+++ press-buttons.py
@@ -3,10 +3,10 @@
 from microbit import*
 
 while True:
-		if button_a.is_pressed() and button_b.is_pressed():
-			display.show(Image.ANGRY)
-		elif button_a.is_pressed():
-			display.show(Image.HAPPY)
-		elif button_b.is_pressed():
-			display.show(Image.SAD)
-		sleep(100)
+	if button_a.is_pressed() and button_b.is_pressed():
+		display.show(Image.ANGRY)
+	elif button_a.is_pressed():
+		display.show(Image.HAPPY)
+	elif button_b.is_pressed():
+		display.show(Image.SAD)
+	sleep(100)
